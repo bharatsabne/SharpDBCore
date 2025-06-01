b1381012d5db6291056c170a637b647fc56a4c2b
bharatsabne <bharatsabne1991@gmail.com>
2025-06-01 14:18:08 +0530

    basic test added

---
+++ SharpDBCore.Tests/DatabaseManagerTests.cs
@@ -0,0 +1,63 @@
+using SharpDBCore.Core;
+using SharpDBCore.Factories;
+using SharpDBCore.Loggers;
+using System.Data.Common;
+
+namespace SharpDBCore.Tests
+{
+    public class DatabaseManagerTests
+    {
+        private const string TestConnectionString = "Server=JITUND-0064\\SQLEXPRESS;Database=TestDb;Integrated Security=True;TrustServerCertificate=True;";
+        public DatabaseManagerTests()
+        {
+            DbConnectionFactory.SetConnectionString(TestConnectionString);
+            var db = DatabaseManager.Instance;
+            db.SetLogger(new NullLogger());
+        }
+        [Fact]
+        public async Task ExecuteScalarAsync_ShouldReturn_1_WhenQuerySelects1()
+        {
+            // Arrange
+            var db = DatabaseManager.Instance;
+            string query = "SELECT 1";
+
+            // Act
+            var result = await db.ExecuteScalarAsync(query);
+
+            // Assert
+            Assert.Equal(1, Convert.ToInt32(result));
+        }
+
+        [Fact]
+        public async Task ExecuteNonQueryAsync_ShouldWork_WithTempTable()
+        {
+            var db = DatabaseManager.Instance;
+            db.SetLogger(new NullLogger());
+
+            db.BeginTransaction();
+
+            try
+            {
+                await db.ExecuteNonQueryAsync("CREATE TABLE #Temp(Id INT)");
+                await db.ExecuteNonQueryAsync("INSERT INTO #Temp(Id) VALUES (@id)", new Dictionary<string, object>
+                    {
+                        {"@id", 99}
+                    });
+
+                var result = await db.ExecuteScalarAsync("SELECT COUNT(*) FROM #Temp");
+
+                Assert.Equal(1, Convert.ToInt32(result));
+
+                var id = await db.ExecuteScalarAsync("SELECT Id FROM #Temp");
+                Assert.Equal(99, Convert.ToInt32(id));
+                db.CommitTransaction();
+            }
+            catch
+            {
+                db.RollbackTransaction();
+                throw;
+            }
+
+        }
+    }
+}
